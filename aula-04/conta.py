07e571d14ff605e4169fd519b385ba2e859b8b0f
Laís Urano <95968249+uranolais@users.noreply.github.com>
2022-12-01 15:12:46 -0300

    Adicionando arquivos da aula04

---
+++ aula-04/conta.py
@@ -0,0 +1,30 @@
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ...{}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo,self.__titular))
+    
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor,destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+
+
+
+
+
+
+    
